395c4971169a677d024f0ec028e7e428438b28c7
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2019-10-19 12:08:30 +0200

    Add ARCore to dependencies
    - Include CameraPermissionHelper to ask for camera permissions on startup
    - Add required manifest entries

--- app/src/main/java/com/gihub/wakayapp/wakaya/MainActivity.java
+++ app/src/main/java/com/gihub/wakayapp/wakaya/MainActivity.java
@@ -3,11 +3,24 @@ package com.gihub.wakayapp.wakaya;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
     }
 }
