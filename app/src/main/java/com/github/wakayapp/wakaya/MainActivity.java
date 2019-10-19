819183bd8f916c4d6ee6f986a27374c9749bd7bc
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2019-10-19 19:49:15 +0200

    WIP

--- app/src/main/java/com/github/wakayapp/wakaya/MainActivity.java
+++ app/src/main/java/com/github/wakayapp/wakaya/MainActivity.java
@@ -2,12 +2,31 @@ package com.github.wakayapp.wakaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.MainActivityTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeToArFindActivity(final View view) {
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_ar_find);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
     }
 }
